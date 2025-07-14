e93d8305967264cf82f92fafc6b8a8e7b0e8bf5f
Baltazar Christian <121530115+T4z4r@users.noreply.github.com>
2025-07-14 12:37:39 +0300

    test

--- app/Http/Controllers/TestPlanController.php
+++ app/Http/Controllers/TestPlanController.php
@@ -29,7 +29,7 @@ class TestPlanController extends Controller
     public function show(TestPlan $testPlan)
     {
         $testPlan->load('testCases', 'project', 'creator', 'assignee');
-        dd($testPlan->project->systems->pluck('system_id'));
+        dd($testPlan->project->systems);
         $features = Feature::whereIn('system_id', $testPlan->project->systems->pluck('system_id'))->get();
         return view('test_plans.view', compact('testPlan', 'features'));
     }
